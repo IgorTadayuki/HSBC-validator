ba5caed32f7f9b6dbfdb384e4c84399b07d2070b
Gabriel Braga <gabriel_o_braga@hotmail.com>
2016-09-27 20:31:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,19 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: gabriel braga
+ * User: Gabriel braga e Igor Tadaiuki
  * Date: 21/09/2016
- * Time: 17:44
+ * Time: 17:36
  */
-
 declare (strict_types=1);
+use bancos\AllBank;
 
-namespace bancos;
-
-class AllBank
-{
-    protected $ValueMultHSBC = "8923456789";
-
-    protected $soma = 0;
-    protected $Digito = 0;
-    protected $valor;
-    protected $i;
-
-    protected $nbanco;
-    protected $nconta;
-    protected $odigito;
+$banco = new AllBank("0416" , "01208" , 2);
+echo $banco->getAgencia();
+echo "nao da";
 
 
-    public function __construct (string $agencia ,string $conta ,int $digito)
-    {
-        $this->nbanco = $agencia;
-        $this->nconta = $conta;
-        $this->odigito = $digito;
-
-    }
-
-    public function getAgencia(){
-        return $this->nconta;
-    }
-
-
-    public function SomaHSBC()
-    {
-
-        $this->Digito = $this->soma % 11;
-    }
-
-    public function Verificar(int $digito)
-    {
-        $this->SomaHSBC($this->soma);
-
-        if ($this->Digito == $digito) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+function __autoload ($class){
+    $class = str_replace('\\','/',$class).'.php';
+    require_once ($class);
 }
